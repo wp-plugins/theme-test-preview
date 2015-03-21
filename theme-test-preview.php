50e29f8bdb5642cd138381ca3ada5591b5d3b836
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-21 07:43:52 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/theme-test-preview/trunk@1117354 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme-test-preview.php
+++ theme-test-preview.php
@@ -7,47 +7,48 @@ Version: 1.2
 */ //FREE LICENCE. Many thanks to "Theme Test Drive" plugin.
 
 
-define('TTPRW_ACTIVE_THEMFOLD',esc_attr(wp_get_theme()->template));  //get_stylesheet()
-define('TTPRW_ACTIVE_THEMNAME',esc_attr(wp_get_theme()->name));  //wp_get_theme()->template
+define('TTPRW_ACTIVE_THEMFOLD',esc_attr(wp_get_theme()->template));	//get_stylesheet()
+define('TTPRW_ACTIVE_THEMNAME',esc_attr(wp_get_theme()->name));		//wp_get_theme()->template
 
-register_deactivation_hook( __FILE__,	'TTPRW_activation');	function TTPRW_activation()		{
-	//
-}
-register_activation_hook( __FILE__,		'TTPRW_deactivation');	function TTPRW_deactivation()	{
-	setcookie('tPREW_t','deleted',	time()-9999999,'/');
-}
+
+
+register_deactivation_hook(__FILE__,'TTPRW_activatt');	function TTPRW_activatt()	{}
+register_activation_hook(__FILE__,  'TTPRW_deactivatt');function TTPRW_deactivatt()	{setcookie('tPREW_t','delet',time()-9999999,'/');}
 	
 	
-	
-function TTPRW_detection(){
+	function TTPRW_detectionn(){
 		if (substr($_SERVER['REQUEST_URI'],-9)=='/testmode')	{header("location: ".home_url().'/?turnTestOffOn='.TTPRW_ACTIVE_THEMFOLD) or die(__FILE__);} 
-	} TTPRW_detection(); //add_action('plugins_loaded','TTPRW_detection');     <-- this doesnt work.. i dont know why..
+	} TTPRW_detectionn(); //add_action('plugins_loaded','TTPRW_detectionn');     <-- this doesnt work.. i dont know why..
 
-function TTPRW_permisionn(){
-		if (get_option('TTPRW_environment_access')=='adminsss')	{if (current_user_can( 'edit_posts' )){return true;} else {return false;}}
-		else {return true;}
+	function TTPRW_permisionn(){
+		if (get_option('TTPRW_environment_access')=='adminsss' && !current_user_can( 'edit_posts' )){return false;}
+		return true;
 	}
 	
 
 add_action('plugins_loaded','TTPRW_change_func');function TTPRW_change_func(){
 	if (TTPRW_permisionn()){
-		if 	(isset($_GET['turnTestOffOn'])) {			//change was detected
-			$GLOBALS['previewMODEE']=$_GET['turnTestOffOn'];	setcookie('tPREW_t',$_GET['turnTestOffOn'],	time()+9999999,'/');
+		//DETECT situation
+		if 	(isset($_GET['turnTestOffOn'])) {				//change was detected
+			$GLOBALS['previewMODEE']=$_GET['turnTestOffOn'];
+			setcookie('tPREW_t',$_GET['turnTestOffOn'],	time()+9999999,'/');
 			header("location: ".home_url()) or die(__FILE__);
 		}
-		elseif ($cookieee_set = $_COOKIE['tPREW_t']){ 		//change was NOT detected
+		elseif (isset($_COOKIE['tPREW_t'])){ 		//change was NOT detected
 			$GLOBALS['previewMODEE']=$_COOKIE['tPREW_t'];
 		}
+		else {
+			setcookie('tPREW_t','delet',	time()-9999999,'/');
+		}
 	}
-}
 
-add_action('plugins_loaded','TTD_filters3');  function TTD_filters3 () {
+	//if enabled, then...
 	if (!empty($GLOBALS['previewMODEE'])){  
  		add_filter('template', 'themedrive_get_template3');
  		add_filter('stylesheet', 'themedrive_get_stylesheet3');
 		//my addition:
-		add_filter( 'option_template', 'TTPRW_determine' );
-		add_filter( 'option_stylesheet', 'TTPRW_determine' );
+		add_filter( 'option_template', 'themedrive_get_template3' );
+		add_filter( 'option_stylesheet', 'themedrive_get_stylesheet3' );
 	}
 }
  
@@ -57,7 +58,7 @@ function themedrive_get_stylesheet3($stylesheet){$theme=TTPRW_determine(); if($t
 function TTPRW_determine(){
   $theme = $GLOBALS['previewMODEE'];	if (empty($theme) || (!file_exists(get_theme_root().'/'.$theme))  ) {return false;}
   //if chosen theme name
-  $theme_data = wp_get_theme($theme);	if ('publish' == $theme_data['Status']) {return $theme_data;} else { return false;}
+  $theme_data = wp_get_theme($theme);	if (!empty($theme_data)) { return ('publish' == $theme_data['Status']) ? $theme_data : false; }
   //if chosen theme title
   $themes = wp_get_themes();
 			foreach ($themes as $theme_data) {
@@ -87,7 +88,7 @@ add_action('admin_menu', 'prev_menuuu_link');function prev_menuuu_link() {add_su
 	?> 
 
 	<div class="choose_theme"><br/><br/>
-	<b>Keep in mind, if you will have problems, just deactivate/delete this plugin. (There exist other relative plugins "Theme Test drive" , "User Theme", "Theme Switch and Preview" , "page theme" and etc..)</b><br/><br/><br/>
+	<b>Keep in mind, if you will have problems, just deactivate/delete this plugin. (There exist other alike plugins "Theme Test drive" , "User Theme", "Theme Switch and Preview" , "page theme" and etc..)</b><br/><br/><br/>
 	<form action="" method="POST">
 	<p> Only Logged in Administrators can see Testing Environment ? <input type="hidden" name="accessts" value="everyonee" /> <input type="checkbox" name="accessts" value="adminsss" <?php if (get_option('TTPRW_environment_access')=='adminsss') {echo 'checked="checked"';}?> />	</p>  <input type="submit" value="Save">  <p>after saving, just visit <a href="<?php echo home_url();?>/testmode" target="_blank" style="color:red;font-size:1.2em;">yoursite.com/<b style="font-size:1.2em;">testmode</b></a> (and on the left upper corner you will see a menu)</p>
 	</form>
@@ -97,7 +98,7 @@ add_action('admin_menu', 'prev_menuuu_link');function prev_menuuu_link() {add_su
 
 add_action('wp_footer','TTPRW_show_testONOFF');function TTPRW_show_testONOFF(){
 	if (!empty($GLOBALS['previewMODEE'])) {
-		echo '<select class="testingCHOOSER" onchange="OnOffTest(this)"
+		echo '<select class="testingCHOOSER" onchange="OnOffTTPRW(this)"
 		style="display:block; z-index:9999; position:fixed;top:30px;width:200px; height:40px;left:1px; padding:4px;color:white; background-color: #C00;border: 5px solid green;" >';
 		
 				$themes = wp_get_themes();
@@ -116,8 +117,7 @@ add_action('wp_footer','TTPRW_show_testONOFF');function TTPRW_show_testONOFF(){
 				}else {echo '<option value="x">NO ADDITIONAL THEMES INSTALLED</option>';}
 				echo '<option value="">--EXIT PREVIEW MODE--</option>';
 				
-		echo '</select>
-		<script type="text/javascript">function OnOffTest(elm){window.location="'.home_url().'/?turnTestOffOn=" + elm.value;}</script>';
+		echo '</select> 	<script type="text/javascript">function OnOffTTPRW(elm){window.location="'.home_url().'/?turnTestOffOn=" + elm.value;}</script>';
 	}
 }
 
